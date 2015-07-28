7c4cf5118f0ad8af2814ca603388a2bcc9bed577
Dima Krasner <dima@dimakrasner.com>
2015-07-29 00:06:31 +0300

    Initial commit.

---
+++ waive.c
@@ -0,0 +1,199 @@
+/*
+ * this file is part of libwaive.
+ *
+ * Copyright (c) 2015 Dima Krasner
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include <sys/socket.h>
+#include <sys/mman.h>
+#include <errno.h>
+
+#include <seccomp.h>
+
+#include "waive.h"
+
+int waive(const int flags)
+{
+	scmp_filter_ctx ctx;
+	int ret = -1;
+
+	ctx = seccomp_init(SCMP_ACT_ALLOW);
+	if (!ctx)
+		goto out;
+
+	if (flags & WAIVE_INET) {
+		if (!seccomp_rule_add(ctx,
+		                      SCMP_ACT_ERRNO(EPERM),
+		                      SCMP_SYS(socket),
+		                      1,
+		                      SCMP_A0(SCMP_CMP_EQ, AF_INET)))
+			goto out;
+
+		if (!seccomp_rule_add(ctx,
+		                      SCMP_ACT_ERRNO(EPERM),
+		                      SCMP_SYS(socket),
+		                      1,
+		                      SCMP_A0(SCMP_CMP_EQ, AF_INET6)))
+			goto out;
+	}
+
+	if (flags & WAIVE_UN) {
+		if (0 != seccomp_rule_add(ctx,
+		                          SCMP_ACT_ERRNO(EPERM),
+		                          SCMP_SYS(socket),
+		                          1,
+		                          SCMP_A0(SCMP_CMP_EQ, AF_UNIX)))
+			goto out;
+	}
+
+	if (flags & WAIVE_PACKET) {
+		if (!seccomp_rule_add(ctx,
+		                      SCMP_ACT_ERRNO(EPERM),
+		                      SCMP_SYS(socket),
+		                      1,
+		                      SCMP_A0(SCMP_CMP_EQ, AF_PACKET)))
+			goto out;
+	}
+
+	if (flags & WAIVE_MOUNT) {
+		if (!seccomp_rule_add(ctx,
+		                      SCMP_ACT_ERRNO(EPERM),
+		                      SCMP_SYS(mount),
+		                      0))
+			goto out;
+
+		if (!seccomp_rule_add(ctx,
+		                      SCMP_ACT_ERRNO(EPERM),
+		                      SCMP_SYS(umount),
+		                      0))
+			goto out;
+
+		if (!seccomp_rule_add(ctx,
+		                      SCMP_ACT_ERRNO(EPERM),
+		                      SCMP_SYS(umount2),
+		                      0))
+			goto out;
+	}
+
+	if (flags & WAIVE_OPEN) {
+		if (!seccomp_rule_add(ctx,
+		                      SCMP_ACT_ERRNO(EPERM),
+		                      SCMP_SYS(open),
+		                      0))
+			goto out;
+
+		if (!seccomp_rule_add(ctx,
+		                      SCMP_ACT_ERRNO(EPERM),
+		                      SCMP_SYS(openat),
+		                      0))
+			goto out;
+
+		if (!seccomp_rule_add(ctx,
+		                      SCMP_ACT_ERRNO(EPERM),
+		                      SCMP_SYS(creat),
+		                      0))
+			goto out;
+	}
+
+	if (flags & WAIVE_EXEC) {
+		if (!seccomp_rule_add(ctx,
+		                      SCMP_ACT_ERRNO(EPERM),
+		                      SCMP_SYS(execve),
+		                      0))
+			goto out;
+
+		if (!seccomp_rule_add(ctx,
+		                      SCMP_ACT_ERRNO(EPERM),
+		                      SCMP_SYS(mprotect),
+		                      1,
+		                      SCMP_A2(SCMP_CMP_EQ, PROT_EXEC)))
+			goto out;
+
+		if (!seccomp_rule_add(ctx,
+		                      SCMP_ACT_ERRNO(EPERM),
+		                      SCMP_SYS(mprotect),
+		                      1,
+		                      SCMP_A2(SCMP_CMP_EQ, PROT_READ | PROT_EXEC)))
+			goto out;
+
+		if (!seccomp_rule_add(ctx,
+		                      SCMP_ACT_ERRNO(EPERM),
+		                      SCMP_SYS(mprotect),
+		                      1,
+		                      SCMP_A2(SCMP_CMP_EQ, PROT_WRITE | PROT_EXEC)))
+			goto out;
+
+		if (!seccomp_rule_add(ctx,
+		                      SCMP_ACT_ERRNO(EPERM),
+		                      SCMP_SYS(mprotect),
+		                      1,
+		                      SCMP_A2(SCMP_CMP_EQ,
+		                              PROT_READ | PROT_WRITE | PROT_EXEC)))
+			goto out;
+
+		if (!seccomp_rule_add(ctx,
+		                      SCMP_ACT_ERRNO(EPERM),
+		                      SCMP_SYS(mmap),
+		                      1,
+		                      SCMP_A2(SCMP_CMP_EQ, PROT_READ | PROT_EXEC)))
+			goto out;
+
+		if (!seccomp_rule_add(ctx,
+		                      SCMP_ACT_ERRNO(EPERM),
+		                      SCMP_SYS(mmap),
+		                      1,
+		                      SCMP_A2(SCMP_CMP_EQ, PROT_WRITE | PROT_EXEC)))
+			goto out;
+
+		if (!seccomp_rule_add(ctx,
+		                      SCMP_ACT_ERRNO(EPERM),
+		                      SCMP_SYS(mmap),
+		                      1,
+		                      SCMP_A2(SCMP_CMP_EQ,
+		                              PROT_READ | PROT_WRITE | PROT_EXEC)))
+			goto out;
+	}
+
+	if (flags & WAIVE_CLONE) {
+		if (!seccomp_rule_add(ctx,
+		                      SCMP_ACT_ERRNO(EPERM),
+		                      SCMP_SYS(clone),
+		                      0))
+			goto out;
+	}
+
+	if (flags & WAIVE_KILL) {
+		if (!seccomp_rule_add(ctx,
+		                      SCMP_ACT_ERRNO(EPERM),
+		                      SCMP_SYS(kill),
+		                      1,
+		                      SCMP_A0(SCMP_CMP_NE, 0)))
+			goto out;
+	}
+
+	if (0 == seccomp_load(ctx))
+		ret = 0;
+
+	seccomp_release(ctx);
+
+out:
+	return ret;
+}
---
+++ waive.h
@@ -0,0 +1,39 @@
+/*
+ * this file is part of libwaive.
+ *
+ * Copyright (c) 2015 Dima Krasner
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef _WAIVE_H_INCLUDED
+#	define _WAIVE_H_INCLUDED
+
+enum waive_flags {
+	WAIVE_INET		= 1 << 0,
+	WAIVE_UN		= 1 << 1,
+	WAIVE_PACKET	= 1 << 2,
+	WAIVE_MOUNT		= 1 << 3,
+	WAIVE_OPEN		= 1 << 4,
+	WAIVE_EXEC		= 1 << 5,
+	WAIVE_CLONE		= 1 << 6,
+	WAIVE_KILL		= 1 << 7
+};
+
+#endif
